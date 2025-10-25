e7832e07b091e9420704c78c3da58485ee086545
Victor Prokhorov <mail@victorprokhorov.com>
2025-10-25 21:49:38 +0200

    add documentation links

---
+++ termie/target/debug/deps/termie-09ec6eb1b443d004.d
@@ -0,0 +1,5 @@
+/home/victorprokhorov/terminal/termie/target/debug/deps/termie-09ec6eb1b443d004.d: src/main.rs
+
+/home/victorprokhorov/terminal/termie/target/debug/deps/libtermie-09ec6eb1b443d004.rmeta: src/main.rs
+
+src/main.rs:
---
+++ termie/target/debug/deps/termie-d0879f88a1a6dced.d
@@ -0,0 +1,5 @@
+/home/victorprokhorov/terminal/termie/target/debug/deps/termie-d0879f88a1a6dced.d: src/main.rs
+
+/home/victorprokhorov/terminal/termie/target/debug/deps/libtermie-d0879f88a1a6dced.rmeta: src/main.rs
+
+src/main.rs:
