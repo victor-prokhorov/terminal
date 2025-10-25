7df6a36c8dd9f79ac8685d3eb05fde2925b636ca
Victor Prokhorov <mail@victorprokhorov.com>
2025-10-26 00:28:16 +0200

    spawn broken shell

--- termie/src/main.rs
+++ termie/src/main.rs
@@ -1,27 +1,57 @@
 use eframe::egui;
+use nix::pty::ForkptyResult;
+use std::{
+    ffi::{CStr, CString},
+    fs::File,
+    io::Read,
+    os::fd::OwnedFd,
+};
 
 fn main() {
+    let fd = unsafe {
+        let result = nix::pty::forkpty(None, None).expect("pty fork failed");
+        match result {
+            ForkptyResult::Parent { master, .. } => master,
+            ForkptyResult::Child => {
+                let shell = CStr::from_bytes_with_nul(b"bash\0").expect("nul termination missing");
+                assert_eq!(shell, c"bash"); // TODO: use CStr directly
+                nix::unistd::execvp::<CString>(shell, &[])
+                    .expect("failed to replace current process image");
+                return; // TODO: find better way
+            }
+        }
+    };
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("App", native_options, Box::new(|cc| Ok(Box::new(App::new(cc)))));
+    let _ = eframe::run_native(
+        "App",
+        native_options,
+        Box::new(|cc| Ok(Box::new(App::new(cc, fd)))),
+    );
 }
 
-#[derive(Default)]
-struct App {}
+struct App {
+    file: File,
+    buf: Vec<u8>,
+}
 
 impl App {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-        Self::default()
+    fn new(_cc: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
+        App {
+            file: fd.into(),
+            buf: Vec::new(),
+        }
     }
 }
 
 impl eframe::App for App {
-   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Hello World!");
-       });
-   }
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let mut buf = vec![0; 4096];
+        match self.file.read(&mut buf) {
+            Err(e) => eprintln!("failed to read: {e}"),
+            Ok(read) => self.buf.extend_from_slice(&buf[0..read]),
+        }
+        egui::CentralPanel::default().show(ctx, |ui| unsafe {
+            ui.label(std::str::from_utf8_unchecked(&self.buf))
+        });
+    }
 }
