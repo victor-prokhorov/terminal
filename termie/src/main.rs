b36fda4a0e96920a0d1dae41afb0656fa7ce92a7
Victor Prokhorov <mail@victorprokhorov.com>
2025-10-26 12:23:17 +0100

    set non blocking flag

--- termie/src/main.rs
+++ termie/src/main.rs
@@ -1,10 +1,17 @@
 use eframe::egui;
-use nix::pty::ForkptyResult;
-use std::{env, ffi::CString, fs::File, io::Read, os::fd::OwnedFd};
+use nix::{
+    fcntl::{FcntlArg, OFlag, fcntl},
+    pty::ForkptyResult,
+};
+use std::{
+    env,
+    ffi::CString,
+    os::fd::{AsFd, OwnedFd},
+};
 
 fn main() {
     let fd = unsafe {
-        let result = nix::pty::forkpty(None, None).expect("pty fork failed");
+        let result = nix::pty::forkpty(None, None).expect("failed to fork pty");
         match result {
             ForkptyResult::Parent { master, .. } => master,
             ForkptyResult::Child => {
@@ -28,14 +35,20 @@ fn main() {
 }
 
 struct App {
-    file: File,
+    fd: OwnedFd,
     buf: Vec<u8>,
 }
 
 impl App {
     fn new(_cc: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
+        let flags =
+            fcntl(fd.as_fd(), FcntlArg::F_GETFL).expect("failed to get descriptor satus flags");
+        let mut flags = OFlag::from_bits(flags)
+            .expect("failed to create configuration options for opened file");
+        flags.set(OFlag::O_NONBLOCK, true);
+        fcntl(fd.as_fd(), FcntlArg::F_SETFL(flags)).expect("failed to set descriptor status flags");
         App {
-            file: fd.into(),
+            fd,
             buf: Vec::new(),
         }
     }
@@ -44,7 +57,7 @@ impl App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let mut buf = vec![0; 4096];
-        match self.file.read(&mut buf) {
+        match self.fd.read(&mut buf) {
             Err(e) => eprintln!("failed to read: {e}"),
             Ok(read) => self.buf.extend_from_slice(&buf[0..read]),
         }
